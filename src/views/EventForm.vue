93169455d1ed3fd4b32c436bc4e0482f5e306cd3
POOM <nitipoom_t@cmu.ac.th>
2022-10-14 18:26:11 +0700

    2.3

--- src/views/EventForm.vue
+++ src/views/EventForm.vue
@@ -28,6 +28,8 @@
         v-model="event.organizer.id"
         label="Select an Organizer"
       />
+      <h3>The image of the Event</h3>
+      <UploadImages @changed="handleImages" />
 
       <button type="submit">Submit</button>
     </form>
@@ -38,8 +40,12 @@
 
 <script>
 import EventService from '@/services/EventService.js'
+import UploadImages from 'vue-upload-drop-images'
 export default {
   inject: ['GStore'],
+  components: {
+    UploadImages
+  },
 
   data() {
     return {
@@ -70,6 +76,9 @@ export default {
         .catch(() => {
           this.$router.push('NetworkError')
         })
+    },
+    handleImages(files) {
+      console.log(files)
     }
   }
 }
